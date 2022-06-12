e553695498ab3b043b300d07f5d1634ce516ff49
EdmerFco <83522812+EdmerFco@users.noreply.github.com>
2022-06-12 12:29:57 -0500

    03-webpack-inicial
    webpack-inicial

---
+++ 03-webpack-inicial/src/js/componentes.js
@@ -0,0 +1,11 @@
+const saludar = (nombre) => {
+
+    console.log('Create etiqueta h1');
+
+    const h1 = document.createElement('h1');
+
+    h1.innerText= `Hola, ${nombre}`;
+
+
+    document.body.append(h1);
+}
